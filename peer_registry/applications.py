05db63adfc19b8101f857a2c2c49d676af7fe2f1
Peer Xu <pppeerxu@gmail.com>
2015-12-14 23:42:22 +0800

    format paramers

--- peer_registry/applications.py
+++ peer_registry/applications.py
@@ -8,36 +8,36 @@ from peer_registry.lib import storage
 store = storage.load()
 
 
-@app.route('/v1/applications/<application_id>/layer', methods=['GET'])
-def get_application_layer(application_id):
-    return Response(stream_with_context(store.stream_read(store.application_layer_path(application_id))))
+@app.route('/v1/applications/<app_id>/layer', methods=['GET'])
+def get_application_layer(app_id):
+    return Response(stream_with_context(store.stream_read(store.application_layer_path(app_id))))
 
 
-@app.route('/v1/applications/<application_id>/layer', methods=['PUT'])
-def put_application_layer(application_id):
+@app.route('/v1/applications/<app_id>/layer', methods=['PUT'])
+def put_application_layer(app_id):
     return response('', 204)
 
 
-@app.route('/v1/applications/<application_id>/json', methods=['GET'])
-def get_application_json(application_id):
-    return response(store.get_content(store.application_json_path(application_id)), raw=True)
+@app.route('/v1/applications/<app_id>/json', methods=['GET'])
+def get_application_json(app_id):
+    return response(store.get_content(store.application_json_path(app_id)), raw=True)
 
 
-@app.route('/v1/applications/<application_id>/json', methods=['PUT'])
-def put_application_json(application_id):
+@app.route('/v1/applications/<app_id>/json', methods=['PUT'])
+def put_application_json(app_id):
     return response('', 204)
 
 
-@app.route('/v1/applications/<application_id>/checksum', methods=['PUT'])
-def put_application_checksum(application_id):
+@app.route('/v1/applications/<app_id>/checksum', methods=['PUT'])
+def put_application_checksum(app_id):
     return response('', 204)
 
 
-@app.route('/v1/applications/<application_id>/checksum', methods=['GET'])
-def get_application_checksum(application_id):
-    return response(store.get_content(store.application_checksum_path(application_id)), raw=True)
+@app.route('/v1/applications/<app_id>/checksum', methods=['GET'])
+def get_application_checksum(app_id):
+    return response(store.get_content(store.application_checksum_path(app_id)), raw=True)
 
 
-@app.route('/v1/applications/<application_id>/ancestry', methods=['GET'])
-def get_application_ancestry(application_id):
-    return response(store.get_content(store.application_ancestry_path(application_id)), raw=True)
+@app.route('/v1/applications/<app_id>/ancestry', methods=['GET'])
+def get_application_ancestry(app_id):
+    return response(store.get_content(store.application_ancestry_path(app_id)), raw=True)
